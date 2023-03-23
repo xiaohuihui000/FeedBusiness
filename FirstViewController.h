2826f3f539347a77e642d6cb613f12a887ad01cd
guanghui10 <guanghui10@staff.sina.com.cn>
2023-03-23 12:15:35 +0800

    第三次

---
+++ FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  FeedBusiness
+//
+//  Created by 陈光辉 on 2023/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstViewController.m
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.m
+//  FeedBusiness
+//
+//  Created by 陈光辉 on 2023/3/23.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
